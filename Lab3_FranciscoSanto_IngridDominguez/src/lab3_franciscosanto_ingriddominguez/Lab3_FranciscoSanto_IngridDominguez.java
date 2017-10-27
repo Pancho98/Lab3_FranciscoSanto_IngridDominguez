6ea9466d8edd8b8f989dc8d48e032519ca0f5023
Francisco Santos <fjgsantos98@gmail.com>
2017-10-27 14:42:21 -0600

    Menu del main

--- Lab3_FranciscoSanto_IngridDominguez/src/lab3_franciscosanto_ingriddominguez/Lab3_FranciscoSanto_IngridDominguez.java
+++ Lab3_FranciscoSanto_IngridDominguez/src/lab3_franciscosanto_ingriddominguez/Lab3_FranciscoSanto_IngridDominguez.java
@@ -5,17 +5,64 @@
  */
 package lab3_franciscosanto_ingriddominguez;
 
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Francisco Santos
  */
 public class Lab3_FranciscoSanto_IngridDominguez {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        ArrayList<Clientes> clientes = new ArrayList();
+        ArrayList<Empleado> empleados = new ArrayList();
+        String resp = "s";
+        while (resp.equals("s") || resp.equals("S")) {
+            String opc = JOptionPane.showInputDialog("a. Log in\n"
+                    + "b. \n"
+                    + "c. \n"
+                    + "d. \n"
+                    + "e. \n"
+                    + "");
+            switch (opc) {
+                case "a":
+                    String Username = JOptionPane.showInputDialog("Ingrese Su username: ");
+                    String Contraseña = JOptionPane.showInputDialog("Ingrese Su contraseña: ");
+                    if (User_Socio.equals(Username) && User_contra.equals(Contraseña)) {
+                        String opcS = JOptionPane.showInputDialog("a. Crear Local\n"
+                                + "b. \n"
+                                + "c. \n"
+                                + "d. \n"
+                                + "e. \n"
+                                + "");
+                    }
+                    for (Empleado t : empleados) {
+                        if (Username.equals(t.getUsername()) && Contraseña.equals(t.getContraseña())) {
+                            String opcC = JOptionPane.showInputDialog("a. Log in\n"
+                                    + "b. \n"
+                                    + "c. \n"
+                                    + "d. \n"
+                                    + "e. \n"
+                                    + "");
+                        }
+                    }
+                    for (Clientes t : clientes) {
+                        if (Username.equals(t.getUsername()) && Contraseña.equals(t.getContraseña())) {
+                            String opcE = JOptionPane.showInputDialog("a. Log in\n"
+                                    + "b. \n"
+                                    + "c. \n"
+                                    + "d. \n"
+                                    + "e. \n"
+                                    + "");
+                        }
+                    }
+                    break;
+                case "b":
+                    break;
+            }
+        }
     }
-    
+    static String User_Socio = "Socio";
+    static String User_contra = "1234";
 }
